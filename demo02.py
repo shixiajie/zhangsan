d11d5320b04f3b56eb5c84c717a33cbdf3fba2b5
shixiajie <2560699196@qq.com>
2022-04-27 17:10:43 +0800

    第一次向GitHub提交代码

---
+++ demo02.py
@@ -0,0 +1,28 @@
+3
+
+# a = "张三" # 把张三这个赋值给了名字叫a的变量
+# print(a)
+
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容:",a+b)
+
+
+# print(type(2333))
+# print(type(2.3333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+# a =int(2333)
+# print(type(a))
+
+# a = "OA合适的话化橘红"
+# print(len(a))
+
+a = input("请输入：")
+b = input("请输入: ")
+print("两段字符串的长度：",len(a)+len(b))
