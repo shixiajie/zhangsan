59afb7a6bb43ad54956b19f8a90a968e245b173d
shixiajie <2560699196@qq.com>
2022-04-27 17:23:40 +0800

    22222

--- demo01.py
+++ demo01.py
@@ -1,2 +1,24 @@
 print(1+2)
 print(1+2)
+# a = "张三" # 把张三这个赋值给了名字叫a的变量
+# print(a)
+
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容:",a+b)
+
+
+# print(type(2333))
+# print(type(2.3333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+# a =int(2333)
+# print(type(a))
+
+# a = "OA合适的话化橘红"
+# print(len(a))
