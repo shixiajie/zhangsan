40c081ee1c6b7b8d351c7df5cf6621f76006594e
shixiajie <2560699196@qq.com>
2022-04-28 14:39:43 +0800

    笔记

---
+++ demon03.py
@@ -0,0 +1,122 @@
+# print("hello world")
+
+# print("hello worid")
+# print("hello world") #字符串
+# print(2333) #整数
+# print(2.3333) #小数
+# print(True) #布尔值 True Flase
+# print(()) #元组
+# print([]) #数组
+# print({}) #字典
+# # 单行注释    """"""  多行注释（无论单引号还是双引号）
+# '''长度恍然大悟，汗滴禾下土'''
+
+# print("好好",2333,2.333)
+# print("哈哈"+"嘻嘻") #字符串的拼接
+# print("好好"*100)
+# print((1+2*10-99)/2)
+
+# print(1>3)
+
+# #变量 赋值
+# a = "张三" #把张三这个值赋值给了名叫a的变量，变量必须是小写字母，不用用特殊符号
+# print(a)
+
+# 数据格式的转换
+
+# print(type(23333))
+# print(type('好好'))
+# print(type(2333))
+# print(type(2.3333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a = float(input("请输入:"))
+# b = float(input ("请输入:"))
+# print("获取的内容:",a+b)
+
+
+# a = float("hhh")
+# print(a)
+
+# a = float(input("请输入"))
+# b = float(input("请输入"))
+# print("获取的内容；",a+b)
+
+
+# 
+
+
+# 
+
+
+
+
+#python常用的方法
+#1. print() 将括号里的值打印到运行代码的地方
+# print("My name is shixiajie.")
+# print("I am 22 years old.")
+# print('I like gudangmu')
+
+# 2.input 获取输入
+#变量：给某一个值取的名字 赋值：把一个具体的在交给变量
+# a = "张三"
+# print(a)
+
+# type 读取数据格式
+# print(type("滚滚滚"))  #字符串
+# print(type(2333)) #整数
+# print(type(2.333)) #小数
+# print(type(True)) #布尔值
+# print(type(())) #元组
+# print(type([])) #数组
+# print(type({})) #字典
+
+#python常见的数据类型
+# str '字符串'  引号引起来的数据都是字符串
+# lint "整数"
+# float "小数"
+# bool "布尔值" 只有两个值  True和Flase 用于判断
+# tuple "元组" (())
+# list "数组"  ([])
+# dict "字典" ({})
+
+
+#python的注释
+#号  凡是注释的内容不会运行，代码运行时注释内容会忽略掉
+
+#python的运算符
+#  +  加，-  减，*  乘，/  除，%  余
+
+
+#数据转换的规律
+#1.任何数据都可以转换为字符串
+#2.整数和小数可以互相转换
+#3.字符串转换其他类型的数据，必须满足【长得像】这个条件
+
+# a = 12
+# print(type(str(a)))
+
+# a = float(23)
+# print(type(a))
+
+# a = float("哈哈哈哈")
+# print(type(a))
+
+
+""""通过代码获取两段内容，并且计算他们的长度和"""
+a = input("获取的内容：")
+b = input("获取的内容：")
+print("长度之和：",len(a+b))
+
+
+
+
+
+
+
+
+
+
